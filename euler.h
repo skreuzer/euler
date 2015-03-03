8f759d3c5cf80b2d6d154705cf31a8423fd7ec07
Steven Kreuzer <skreuzer@FreeBSD.org>
2015-03-03 13:48:55 -0500

    Add macros for min and max

--- euler.h
+++ euler.h
@@ -5,6 +5,9 @@
 #define KRED  "\x1B[31m"
 #define KGRN  "\x1B[32m"
 
+#define min(a, b) ((a) < (b) ? (a) : (b))
+#define max(a, b) ((a) > (b) ? (a) : (b))
+
 int check(int, int, int);
 void error(const char *str);
 
